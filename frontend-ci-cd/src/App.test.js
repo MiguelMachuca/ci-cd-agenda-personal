0e7567f3ceaa8049a641f66f5c03f37d7a12a583
Miguel Angel Machuca Yavita <mmachucayavita@gmail.com>
2025-08-31 00:10:22 -0400

    Convertir backend-agenda y frontend-agenda a carpetas normales v2

--- frontend-ci-cd/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders agenda personal app', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Agenda Personal/i);
-  expect(linkElement).toBeInTheDocument();
-});
